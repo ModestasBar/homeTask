7ee7fe1cc82cb90fcb0edeadec6e62a8648288ba
ModestasBar <odekst@gmail.com>
2019-08-16 02:24:57 -0500

    tests

--- test/rules.test.js
+++ test/rules.test.js
@@ -3,3 +3,13 @@ const rules = require('../controller/rules');
 test('function exist', ()=> {
     expect(typeof rules.handleCashInAPI).toEqual('function')
 })
+
+test('checkIn configurations', async()=> {
+    expect(await rules.handleCashInAPI()).toEqual({
+        percents: 0.03,
+        max: {
+        amount: 5,
+        currency: "EUR"
+        }
+    })
+})
