2fe2404eb502c8961a516a7888c135cfcf95717f
ModestasBar <odekst@gmail.com>
2019-08-20 21:57:34 -0500

    code refactor

--- app.js
+++ app.js
@@ -4,42 +4,16 @@ Input:
 Output: 
     Single number according configuration API's.
 */
-const commission = require('./modules/actionEvaluation');
+const action = require('./modules/actionEvaluation');
 const jsonFile = require('./modules/data');
 
 async function main(url) {
     const readUser = jsonFile.handleData(url);
     for(let i = 0; i < readUser.length; i++) {
-       await commission.fee(readUser[i]);
+       await action.fee(readUser[i]);
     };
 };
 
 
 main('repository/input.json');
-
-
-
-// class UserData {
-//     constructor(user) {
-//         this.user = user;
-//         this.limit = false;
-//         this.checkOutHistory = [];
-//         this.test();
-//     };
-
-
-//     test() {
-//         console.log('aasfsdaf');
-//     }
-// }
-
-// let user = new UserData();
-
-// console.log(user);
-
-
-// // const dateTest = require('date-and-time');
-
-// // console.log(dateTest.subtract(new Date('2016-09-19'), new Date('2019-09-18')).toDays());
-
 
--- modules/actionEvaluation.js
+++ modules/actionEvaluation.js
@@ -1,27 +1,28 @@
 const rules = require('../controller/rules');
 const cashIn = require('./cashIn');
-const cashOut = require('./cashOut');
-
+const legalCashOut = require('./cashOut/legal');
+const naturalCashOut = require('./cashOut/natural');
+const currency = 'EUR';
 
 const fee = async user => {
-const acceptingCurrency = 'EUR';
-
-if(user.operation.currency === acceptingCurrency){
-    //Cash in commission calculation
-    if(user.type == 'cash_in') {
-        const cashInAPI = await rules.handleCashInAPI();
-        return console.log(cashIn.commission(cashInAPI, user.operation.amount));
-    }
-    //Cash out commissions fee when user is natural
-    if(user.user_type == 'natural') {
-        const cashOutAPINatural = await rules.handleCashOutAPINatural();
-        return console.log(cashOut.commissionNatural(cashOutAPINatural, user));
-    }
-    //Cash out commissions fee when user is legal
-    const cashOutAPIPersonal = await rules.handleCashOutAPIPersonal();
-    return console.log(cashOut.commissionLegal(cashOutAPIPersonal, user.operation.amount));
-    }       
-return console.log(`Currency error`);
+    if(user.operation.currency === currency){
+        //Cash in commission calculation
+        if(user.type == 'cash_in') {
+            const cashInAPI = await rules.handleCashInAPI();
+            return console.log(cashIn.commission(cashInAPI, user.operation.amount));
+        }
+        //Cash out commissions fee when user is natural
+        if(user.user_type == 'natural') {
+            const cashOutAPINatural = await rules.handleCashOutAPINatural();
+            return console.log(naturalCashOut.commission(cashOutAPINatural, user));
+        }
+        //Cash out commissions fee when user is legal
+        const cashOutAPIPersonal = await rules.handleCashOutAPIPersonal();
+        return console.log(legalCashOut.commission(cashOutAPIPersonal, user.operation.amount));
+        }       
+    return console.log(`Currency error`);
 }
+
+
 
 module.exports = {fee};
